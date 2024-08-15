6f1f81f7102f43f5fe00595dd334ece4a21bfc97
Richar Bolaños <richarbolanos0@gmail.com>
2024-08-15 02:17:17 -0500

    Subir tarea-7

---
+++ Richar-Bolanos-3212393296/Tarea-7/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import CharacterPage from './CharacterPage';
+import EpisodesPage from './EpisodesPage';
+import NotFoundPage from './NotFoundPage';
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/characters" element={<CharacterPage />} />
+          <Route path="/episodes" element={<EpisodesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
